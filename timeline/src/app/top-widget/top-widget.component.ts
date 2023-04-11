c0291a3155af2f345b8fb173f56af238eae42028
Pongpanot Samarkkarn <61924591+pongpanot0@users.noreply.github.com>
2023-04-11 22:53:13 +0700

    s

--- timeline/src/app/top-widget/top-widget.component.ts
+++ timeline/src/app/top-widget/top-widget.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-widget',
   templateUrl: './top-widget.component.html',
-  styleUrls: ['./top-widget.component.css']
+  styleUrls: ['./top-widget.component.scss']
 })
 export class TopWidgetComponent implements OnInit {
 
